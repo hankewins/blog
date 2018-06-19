e6d36ca3ef4fe2685141c819aceb7adcb8ffe792
hankewins <me@hankewins.com>
2018-06-19 21:54:01 +0800

    Site updated: 2018-06-19 21:54:00

---
+++ js/baidu.js
@@ -0,0 +1,14 @@
+/*
+* @Author: hankewins
+* @Date:   2018-06-19 21:41:22
+* @Last Modified by:   hankewins
+* @Last Modified time: 2018-06-19 21:41:41
+*/
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b494e42315911585e7eddac10ea895ff";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
